1f3cca9f99d35fd2d108970f1b69ee34f6a31723
lizhong <mylizhong@126.com>
2017-10-11 18:43:12 +0800

    添加mybatis

---
+++ service/src/main/java/com/wangli/entity/TestUser.java
@@ -0,0 +1,26 @@
+package com.wangli.entity;
+
+import java.io.Serializable;
+
+public class TestUser implements Serializable{
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
