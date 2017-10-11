c2e9044703d35eb2239391b4dd37af8a763bfe3f
lizhong <mylizhong@126.com>
2017-10-11 10:40:08 +0800

    修改springMVC中注入dubbo rpc传输的类爆空的bug

--- service/src/main/java/com/wangli/service/impl/TestServiceImpl.java
+++ service/src/main/java/com/wangli/service/impl/TestServiceImpl.java
@@ -1,11 +1,7 @@
 package com.wangli.service.impl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.wangli.service.TestService;
-import org.springframework.stereotype.Component;
 
-@Component
-@Service
 public class TestServiceImpl implements TestService{
 
     public int testSum(int num1, int num2) {
